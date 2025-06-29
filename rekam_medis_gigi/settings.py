b547df0a8ff92055f01eb5d0c7cbd8f89fbd1342
valentino222 <valentinoangga222@gmail.com>
2025-06-29 20:32:04 +0700

    Menambahkan render.yaml dan konfigurasi deployment

--- rekam_medis_gigi/settings.py
+++ rekam_medis_gigi/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'rekam_medis_gigi.urls'
@@ -117,8 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+ALLOWED_HOSTS += ['*']
+
+# Agar bisa collectstatic tanpa error (jika pakai static files)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
